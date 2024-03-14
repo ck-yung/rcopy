bf1a3d1f28dd8229597bcb6acbf1ce280b43a88e
Yung, Chun Kau <yung.chun.kau@gmail.com>
2024-03-14 23:43:46 +0800

    Fix tab/space in 'Program.cs'

--- Program.cs
+++ Program.cs
@@ -7,36 +7,36 @@ public class Program
 {
     public static void Main(string[] args)
     {
-		try
-		{
-			Log.Init();
+        try
+        {
+            Log.Init();
             _ = RunMainAsync(args);
-		}
-		catch (ArgumentException ae)
-		{
-			WriteLine($"Argument: {ae.Message}");
-		}
-		catch (SocketException se)
-		{
+        }
+        catch (ArgumentException ae)
+        {
+            WriteLine($"Argument: {ae.Message}");
+        }
+        catch (SocketException se)
+        {
             WriteLine($"Socket: {se.Message}");
         }
-		catch (Exception ee)
-		{
-			WriteLine(ee.ToString());
-		}
+        catch (Exception ee)
+        {
+            WriteLine(ee.ToString());
+        }
     }
 
-	static bool RunMainAsync(string[] args)
-	{
-		if (args.Any((it) => "--help" == it))
-		{
+    static bool RunMainAsync(string[] args)
+    {
+        if (args.Any((it) => "--help" == it))
+        {
             return Helper.PrintSyntax(isDetailed:true);
         }
 
         if (args.Length < 2) return Helper.PrintSyntax();
 
-		var commandThe = args[0];
-		var ipThe = args[1];
+        var commandThe = args[0];
+        var ipThe = args[1];
         var argsRest = args.Skip(2).Select((it) => new FlagedArg(false, it));
 
         #region --verbose
@@ -45,23 +45,23 @@ public class Program
         #endregion
 
         switch (commandThe)
-		{
-			case "on":
-				return Server.Run(ipThe, argsRest);
-			case "to":
-				return SendTo(ipThe, argsRest);
-			default:
-				return Helper.PrintSyntax();
-		}
-	}
+        {
+            case "on":
+                return Server.Run(ipThe, argsRest);
+            case "to":
+                return SendTo(ipThe, argsRest);
+            default:
+                return Helper.PrintSyntax();
+        }
+    }
 
-	record FilesFrom(StreamReader Input, Action<StreamReader> CloseAction)
-	{
-		public static FilesFrom Null = new (StreamReader.Null, (_) => { });
-		public void Close()
-		{
-			CloseAction(Input);
-		}
+    record FilesFrom(StreamReader Input, Action<StreamReader> CloseAction)
+    {
+        public static FilesFrom Null = new (StreamReader.Null, (_) => { });
+        public void Close()
+        {
+            CloseAction(Input);
+        }
         public IEnumerable<string> GetPathsFrom()
         {
             while (true)
@@ -74,46 +74,48 @@ public class Program
     }
 
     static bool SendTo(string ipTarget, IEnumerable<FlagedArg> argsRest)
-	{
-		FilesFrom OpenFilesFrom(string path)
-		{
-			if (string.IsNullOrEmpty(path)) return FilesFrom.Null;
-			Log.Verbose($"Files-from is '{path}'");
-			if (path == "-")
-			{
-				if (false == Console.IsInputRedirected)
-				{
+    {
+        FilesFrom OpenFilesFrom(string path)
+        {
+            if (string.IsNullOrEmpty(path)) return FilesFrom.Null;
+            Log.Verbose($"Files-from is '{path}'");
+            if (path == "-")
+            {
+                if (false == Console.IsInputRedirected)
+                {
                     Helper.PrintSyntax();
-					return FilesFrom.Null;
+                    return FilesFrom.Null;
                 }
-                return new FilesFrom(new StreamReader(OpenStandardInput()), (_) => { });
+                return new FilesFrom(new StreamReader(OpenStandardInput()),
+                    (_) => { });
             }
 
-			if (false == File.Exists(path))
-			{
-				WriteLine($"File '{path}' (--files-from) is NOT found!");
+            if (false == File.Exists(path))
+            {
+                WriteLine($"File '{path}' (--files-from) is NOT found!");
                 return new FilesFrom(StreamReader.Null, (_) => { });
             }
-			return new FilesFrom(File.OpenText(path), (it) => it.Close());
-		}
+            return new FilesFrom(File.OpenText(path), (it) => it.Close());
+        }
 
-        (var pathFilesFrom, argsRest) = Options.Parse("--files-from", argsRest, shortcut:"-T");
-		var filesFrom = OpenFilesFrom(pathFilesFrom);
+        (var pathFilesFrom, argsRest) = Options.Parse(
+            "--files-from", argsRest, shortcut:"-T");
+        var filesFrom = OpenFilesFrom(pathFilesFrom);
 
-		var paths = argsRest.Select((it) => it.Arg).ToArray();
-		//Log.Verbose($"paths={string.Join(';',paths)}");
+        var paths = argsRest.Select((it) => it.Arg).ToArray();
+        //Log.Verbose($"paths={string.Join(';',paths)}");
 
-		IEnumerable<Info> infos = filesFrom.GetPathsFrom()
-			.Select((it) => it.Trim())
+        IEnumerable<Info> infos = filesFrom.GetPathsFrom()
+            .Select((it) => it.Trim())
             .Union(paths)
-			.Distinct()
-			.Select((it) => new Info(it, new FileInfo(it)))
-			.Where((it) => it.File.Exists);
+            .Distinct()
+            .Select((it) => new Info(it, new FileInfo(it)))
+            .Where((it) => it.File.Exists);
 
         var taskResult = Client.Run(ipTarget, infos);
-		taskResult.Wait();
+        taskResult.Wait();
         filesFrom.Close();
-		if (1 > taskResult.Result) WriteLine("No file is sent.");
+        if (1 > taskResult.Result) WriteLine("No file is sent.");
         return taskResult.Result > 0;
-	}
+    }
 }
